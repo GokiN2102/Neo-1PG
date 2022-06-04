06ef1572fb81728bfb18ad60338956db94b21985
Aaron <dolphinheavy@gmail.com>
2022-06-04 14:40:55 +0800

    Added bot reply feature

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,25 @@
-import { Client, Intents } from 'discord.js'; 
+import { config } from 'dotenv';
+config({ path: '.env' })
+
+import { Client, Intents, Message } from 'discord.js'; 
+
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 bot.on('ready', () => console.log('ready'));
-bot.login('OTM0MzM2MDEzNDgzMDA4MDQx.YeumOA.en9IXh-yVJyvBcwPAu6qKcCdDW0');
+bot.on('message', async(msg) =>{
+    //if(msg.author.bot) return;
+
+    //await msg.reply("yahoo~")
+    
+    if (msg.author.bot){
+        return 
+    }
+    if (msg.content.startsWith('!Kumi')){
+        msg.reply("Kumi here!!")
+    }
+    if (msg.content.startsWith('!Dkumi')){
+        let msgcontent = msg.content.replace("!Dkumi","")
+        msg.member.send("You have summoned Kumi!!")
+    }
+})
+bot.login(process.env.BOT_TOKEN);
+
